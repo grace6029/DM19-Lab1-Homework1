fdfd5179e8d12b035bcb75b7704b3618aac7a0f6
grace6029 <grace6029@gmail.com>
2019-10-07 16:40:16 +0800

    improve some answer

--- DM19-Lab1-Homework1/helpers/data_mining_helpers.py
+++ DM19-Lab1-Homework1/helpers/data_mining_helpers.py
@@ -10,7 +10,7 @@ Email: ellfae@gmail.com
 def format_rows(docs):
     """ format the text field and strip special characters """
     D = []
-    for d in docs:
+    for d in docs.data:
         temp_d = " ".join(d.split("\n")).strip('\n\t')
         D.append([temp_d])
     return D
@@ -31,13 +31,9 @@ def tokenize_text(text, remove_stopwords=False):
     """
     Tokenize text using the nltk library
     """
-    '''
     tokens = []
     for d in nltk.sent_tokenize(text, language='english'):
         for word in nltk.word_tokenize(d, language='english'):
             # filters here
             tokens.append(word)
-    '''
-    tokenizer = RegexpTokenizer(r'\w+')
-    tokens = tokenizer.tokenize(text)
     return tokens
